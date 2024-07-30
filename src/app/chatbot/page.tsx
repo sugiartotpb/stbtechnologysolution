2503fbcd58a317d98ba26bde03142f2d2f780cf9
Sugiarto Tampubolon <sugiarttampubolon@gmail.com>
2024-07-30 15:44:13 +0700

    Update chat page

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -1,12 +1,11 @@
 import ChatHeader from '@/components/ChatHeader'
-import Chat from '@/components/chat'
 import React from 'react'
 import { useState, useEffect } from 'react'
 
 const ChatPage = () => {
   return (
     <>
-        <Chat/>
+        
     </>
   )
 }
