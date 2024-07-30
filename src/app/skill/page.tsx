ddba0251787b1b8b01dbb1cb2828542feb53af00
Sugiarto Tampubolon <sugiarttampubolon@gmail.com>
2024-07-30 17:02:11 +0700

    Update page skill object

--- src/app/skill/page.tsx
+++ src/app/skill/page.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
-const skills = {
-  'Programming Languages': ['JavaScript', 'TypeScript', '.NET MVC', '.NET Core'],
-  'Front-End Framework': ['React'],
-  'Back-End Framework': ['.NET WEB API'],
-  'Databases': ['MySQL', 'PostgreSQL', 'MS SQL'],
-  'DevOps & Cloud': ['Azure'],
-};
+// const skills = {
+//   'Programming Languages': ['JavaScript', 'TypeScript', '.NET MVC', '.NET Core'],
+//   'Front-End Framework': ['React'],
+//   'Back-End Framework': ['.NET WEB API'],
+//   'Databases': ['MySQL', 'PostgreSQL', 'MS SQL'],
+//   'DevOps & Cloud': ['Azure'],
+// };
+
+const objectskills = [
+        {
+            "skill": "Programming Languages",
+            "child":['JavaScript', 'TypeScript', '.NET MVC', '.NET Core']
+        },
+        {
+            "skill": "Front-End Framework",
+            "child":['React']
+        },
+        {
+            "skill": "Back-End Framework",
+            "child":['.NET WEB API']
+        },
+        {
+            "skill": "Databases",
+            "child":['MySQL', 'PostgreSQL', 'MS SQL']
+        },
+        {
+            "skill": "DevOps & Cloud",
+            "child":['Azure']
+        },
+    ];
 const AboutPage = () => {
   return (
     <>
@@ -13,11 +36,11 @@ const AboutPage = () => {
       <div className="container mx-auto">
         <h2 className="text-3xl font-bold text-center mb-8 icon-orange-block">SKILLS</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {Object.keys(skills).map((category, index) => (
+          {objectskills.map((category, index) => (
             <div key={index} className="bg-white p-6 rounded-lg shadow-lg">
-              <h3 className="text-xl font-semibold mb-4 icon-orange-block">{category}</h3>
+              <h3 className="text-xl font-semibold mb-4 icon-orange-block">{category.skill}</h3>
               <ul className="list-disc list-inside space-y-2">
-                {Object.keys(category).map((item, i) => (
+                {category.child.map((item, i) => (
                   <li key={i} className="text-gray-700">{item}</li>
                 ))}
               </ul>
